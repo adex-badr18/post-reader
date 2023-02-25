dab92ff628a994806ce0b26d04aae47c67497def
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-02-25 04:17:35 +0100

    Use ngOnInit() to add posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/Post';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  title: string = "Posts";
+  posts: Post[] = [];
+
+  ngOnInit(): void {
+    this.posts = [
+      {
+        id: 1,
+        title: 'My first post',
+        body: 'This is my first post.',
+        votes: 1
+      },
+      {
+        id: 2,
+        title: 'My favorite frontend framework',
+        body: 'My favorite frontend framework is Angular.',
+        votes: 1
+      },
+      {
+        id: 1,
+        title: 'My favorite backend framework',
+        body: 'My favorite backend framework is NodeJS and Express.',
+        votes: 1
+      }
+    ];
+  }
 
 }
